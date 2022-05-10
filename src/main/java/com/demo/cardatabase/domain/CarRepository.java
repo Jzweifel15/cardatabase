d20a8790c79f0ed7fa1a65db4fcecadbed8dee6b
Justin R. Zweifel <jzweifel15@yahoo.com>
2022-05-09 21:13:56 -0400

    Update CarRepository.java
    Extend PagingAndSortingRepository since it extends CrudRepository, plus gives additional needed methods

--- src/main/java/com/demo/cardatabase/domain/CarRepository.java
+++ src/main/java/com/demo/cardatabase/domain/CarRepository.java
@@ -1,7 +1,7 @@
 package com.demo.cardatabase.domain;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CarRepository extends CrudRepository<Car, Long> {
+public interface CarRepository extends PagingAndSortingRepository<Car, Long> {
     
 }

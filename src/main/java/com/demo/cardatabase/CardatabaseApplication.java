703472283459e163cbd73ff1425996b0b1378d4e
Justin R. Zweifel <jzweifel15@yahoo.com>
2022-05-09 21:12:30 -0400

    Update CardatabaseApplication.java
    Add dummy data to the database

--- src/main/java/com/demo/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/demo/cardatabase/CardatabaseApplication.java
@@ -2,12 +2,31 @@ package com.demo.cardatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.demo.cardatabase.domain.Car;
+import com.demo.cardatabase.domain.CarRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication {
 
+	@Autowired
+	private CarRepository repository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+
+	@Bean 
+	CommandLineRunner runner () {
+		return args -> {
+			// Save demo data to DB
+			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 33545));
+			repository.save(new Car("Dodge", "Challenger", "Black", "KK0--212", 2019, 38995));
+			repository.save(new Car("Chevrolet", "Camaro", "Blue", "SSJ-3002", 2018, 37000));
+		};
 	}
 
 }

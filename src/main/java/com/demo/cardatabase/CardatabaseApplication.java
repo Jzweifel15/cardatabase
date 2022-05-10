02069df2c1871c0d42d9a29da135559e6dd68793
Justin R. Zweifel <jzweifel15@yahoo.com>
2022-05-09 21:50:09 -0400

    Update CardatabaseApplication.java
    Insert Owners and Cars to into DB to reflect the relationships created between the two entities

--- src/main/java/com/demo/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/demo/cardatabase/CardatabaseApplication.java
@@ -8,12 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.demo.cardatabase.domain.Car;
 import com.demo.cardatabase.domain.CarRepository;
+import com.demo.cardatabase.domain.Owner;
+import com.demo.cardatabase.domain.OwnerRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication {
 
 	@Autowired
-	private CarRepository repository;
+	private CarRepository crepository;
+
+	@Autowired OwnerRepository orepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
@@ -22,10 +26,21 @@ public class CardatabaseApplication {
 	@Bean 
 	CommandLineRunner runner () {
 		return args -> {
-			// Save demo data to DB
-			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 33545));
-			repository.save(new Car("Dodge", "Challenger", "Black", "KK0--212", 2019, 38995));
-			repository.save(new Car("Chevrolet", "Camaro", "Blue", "SSJ-3002", 2018, 37000));
+			// Add owner objects and save to DB
+			Owner owner1 = new Owner("Dominic", "Toretto");
+			Owner owner2 = new Owner("Brian", "O'Conner");
+			orepository.save(owner1);
+			orepository.save(owner2);
+
+			// Add Car object with link to owners and save to DB
+			Car car = new Car("Dodge", "Challenger", "Black", "KK0--212", 2019, 38995, owner1);
+			crepository.save(car);
+
+			car = new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 33545, owner2);
+			crepository.save(car);
+
+			car = new Car("Chevrolet", "Camaro", "Blue", "SSJ-3002", 2018, 37000, owner2);
+			crepository.save(car);
 		};
 	}
 

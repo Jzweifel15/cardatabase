342aa80e33e65fa9b09e879d490fc4ad8d90c2fc
Justin R. Zweifel <jzweifel15@yahoo.com>
2022-05-09 20:36:13 -0400

    Update Car.java
    Constructors, Setters, and Getters added to Car entity

--- src/main/java/com/demo/cardatabase/domain/Car.java
+++ src/main/java/com/demo/cardatabase/domain/Car.java
@@ -14,4 +14,64 @@ public class Car {
     private String brand, model, color, registerNumber;
     private int year, price;
 
+    // Constructors
+    public Car() { }
+    public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registerNumber = registerNumber;
+        this.year = year;
+        this.price = price;
+    } 
+
+    // Getters
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public String getRegisterNumber() {
+        return this.registerNumber;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    // Setters
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
